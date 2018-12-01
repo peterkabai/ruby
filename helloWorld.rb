992ad0b5803f7a07750dfb8d91a07681187d6989
Peter <pkabai@csumb.edu>
2018-12-01 15:23:11 -0800

    Makes actual hello world app

--- helloWorld.rb
+++ helloWorld.rb
@@ -1,18 +1,11 @@
-fileHtml = File.new("helloWorld.html", "w+")
+class Greeting 
+  def initialize(name)
+    @name = name
+  end
+  def print 
+    puts "Hello, #{@name}!"
+  end
+end
 
-time = Time.new
-
-fileHtml.puts "<html>"
-fileHtml.puts "<head>"
-fileHtml.puts "<title>Ruby Test Page</title>"
-fileHtml.puts "</head>"
-fileHtml.puts "<body>"
-fileHtml.puts "Hello, World!"
-fileHtml.puts "<br/>"
-fileHtml.puts "Year:"  , time.year
-fileHtml.puts "<br/>"
-fileHtml.puts "Month:" , time.month
-fileHtml.puts "</body>"
-fileHtml.puts "</html>"
-
-fileHtml.close()
+person = Greeting.new("Peter")
+person.print
---
+++ htmlTest.rb
@@ -0,0 +1,18 @@
+fileHtml = File.new("htmlTest.html", "w+")
+
+time = Time.new
+
+fileHtml.puts "<html>"
+fileHtml.puts "<head>"
+fileHtml.puts "<title>Ruby Test Page</title>"
+fileHtml.puts "</head>"
+fileHtml.puts "<body>"
+fileHtml.puts "Hello, World!"
+fileHtml.puts "<br/>"
+fileHtml.puts "Year:"  , time.year
+fileHtml.puts "<br/>"
+fileHtml.puts "Month:" , time.month
+fileHtml.puts "</body>"
+fileHtml.puts "</html>"
+
+fileHtml.close()
